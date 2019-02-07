de8c69936b6fc5851d52f85e11a51327a6a84777
Manuel Pfemeter <manuel.pfemeter@aitgmbh.de>
2019-02-07 14:41:08 +0100

    adds ci infrastructure

---
+++ build.fsx
@@ -0,0 +1,82 @@
+#r "paket: groupref Build //"
+#load "./.fake/build.fsx/intellisense.fsx"
+
+open Fake.BuildServer
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.JavaScript
+
+BuildServer.install [
+    TeamFoundation.Installer
+]
+
+Target.create "Build.Frontend" (fun _ ->
+    Trace.trace ""
+    Npm.install (fun o ->
+        { o with
+            WorkingDirectory = "./"
+        })
+
+    let angular = ProcessUtils.tryFindFileOnPath  "ng"
+    if angular.IsNone then failwith "angular cli could not be found"
+
+    let angularResult =
+        ["build"; "--prod"]
+        |> CreateProcess.fromRawCommand angular.Value
+        |> CreateProcess.withWorkingDirectory "./src/web"
+        |> Proc.run
+    if angularResult.ExitCode <> 0 then failwith "could not build frontend"
+)
+
+Target.create "Build.Backend" (fun _ ->
+
+    let buildParams (o: DotNet.BuildOptions) =
+        { o with
+            Configuration = DotNet.BuildConfiguration.Release
+        }
+
+    DotNet.build buildParams "./src/web/web.csproj"
+)
+
+Target.create "Publish" (fun _ ->
+    let publishParams (o: DotNet.PublishOptions) =
+        if BuildServer.isLocalBuild then
+            { o with
+                Configuration = DotNet.BuildConfiguration.Release
+            }
+        else
+            { o with
+                Configuration = DotNet.BuildConfiguration.Release
+                OutputPath = Some(Environment.environVarOrFail("BUILD_ARTIFACTSTAGINGDIRECTORY") +  "/server")
+            }
+    DotNet.publish publishParams "./src/web/web.csproj"
+)
+
+let isEmptyDirectory x = (System.IO.Directory.GetFiles x).Length = 0 && (System.IO.Directory.GetDirectories x).Length = 0
+
+let publishArtifacts path artifactName =
+    if not (isEmptyDirectory path) then
+        Trace.publish (ImportData.BuildArtifactWithName artifactName) path
+
+Target.create "DeployArtifacts" (fun _ ->
+    Trace.log "Uploading server artifacts"
+    if not BuildServer.isLocalBuild then
+        publishArtifacts (Environment.environVarOrFail("BUILD_ARTIFACTSTAGINGDIRECTORY") +  "/server") "server"
+    else
+        Trace.log "Skipping uploading server artifacts, because the build is not executed on the build server"
+    ()
+)
+
+Target.create "All" ignore
+
+Target.create "Build" ignore
+
+"Build.Frontend"
+    ==> "Build.Backend"
+    ==> "Build"
+    ==> "Publish"
+    ==> "DeployArtifacts"
+    ==> "All"
+
+Target.runOrDefault "All"
