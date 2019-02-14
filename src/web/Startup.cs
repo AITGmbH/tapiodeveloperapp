424b07778db8cddea94740c3533449354f7838eb
Manuel Pfemeter <manuel.pfemeter@aitgmbh.de>
2019-02-14 09:08:20 +0100

    fixes issues, uses updated rule set

--- src/web/Startup.cs
+++ src/web/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -7,6 +8,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Aitgmbh.Tapio.Developerapp.Web
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Info Code Smell",
+        "S1309:Track uses of in-source issue suppressions", Justification = "<Pending>")]
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -17,14 +20,19 @@ namespace Aitgmbh.Tapio.Developerapp.Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+#pragma warning disable S2325 // Methods and properties that don't access instance data should be static
         public void ConfigureServices(IServiceCollection services)
+#pragma warning restore S2325 // Methods and properties that don't access instance data should be static
         {
-            services.AddMvc()
-                        .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+#pragma warning disable S2325 // Methods and properties that don't access instance data should be static
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+#pragma warning restore S2325 // Methods and properties that don't access instance data should be static
         {
             if (env.IsDevelopment())
             {
@@ -38,7 +46,7 @@ namespace Aitgmbh.Tapio.Developerapp.Web
             app.Use(async (context, next) =>
             {
               var path = context.Request.Path.Value;
-              if (!path.StartsWith("/api") && !path.StartsWith("/hubs") && !Path.HasExtension(path))
+              if (!path.StartsWith("/api", StringComparison.OrdinalIgnoreCase) && !path.StartsWith("/hubs", StringComparison.OrdinalIgnoreCase) && !Path.HasExtension(path))
               {
                   context.Request.Path = "/index.html";
               }
