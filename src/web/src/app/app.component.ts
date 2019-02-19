e04177b4d488512d537aa220cd9f3dd32b6a4a35
Manuel Pfemeter <manuel.pfemeter@aitgmbh.de>
2019-02-19 17:13:12 +0100

    extracts hamburger menu into separate component

--- src/web/src/app/app.component.ts
+++ src/web/src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'developerapp';
-  isHamburgerMenuActive = false;
-
-  toogleHamburgerMenu() {
-    this.isHamburgerMenuActive = !this.isHamburgerMenuActive;
-  }
 }
