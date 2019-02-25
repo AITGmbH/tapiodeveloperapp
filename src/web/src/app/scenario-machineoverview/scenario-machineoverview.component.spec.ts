845f1b88d49ffcac38b0109bc42d3702c97676da
Manuel Pfemeter <manuel.pfemeter@aitgmbh.de>
2019-02-25 14:21:01 +0100

    fixes tests

--- src/web/src/app/scenario-machineoverview/scenario-machineoverview.component.spec.ts
+++ src/web/src/app/scenario-machineoverview/scenario-machineoverview.component.spec.ts
@@ -1,15 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ScenarioMachineoverviewComponent } from './scenario-machineoverview.component';
+import { SharedModule } from '../shared/shared.module';
 
 describe('ScenarioMachineoverviewComponent', () => {
   let component: ScenarioMachineoverviewComponent;
   let fixture: ComponentFixture<ScenarioMachineoverviewComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ScenarioMachineoverviewComponent ]
-    })
+  TestBed.configureTestingModule({
+      declarations: [ScenarioMachineoverviewComponent],
+      imports: [SharedModule, HttpClientTestingModule]
+      })
     .compileComponents();
   }));
 
