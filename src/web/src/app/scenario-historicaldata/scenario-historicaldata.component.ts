2bda74901c80f33ec307ed6a53615272c7202cc2
Lukas Ochsenreiter <Lukas.Ochsenreiter@aitgmbh.de>
2019-03-14 12:14:28 +0100

    Adds busy spinner on change machine

--- src/web/src/app/scenario-historicaldata/scenario-historicaldata.component.ts
+++ src/web/src/app/scenario-historicaldata/scenario-historicaldata.component.ts
@@ -1,40 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of, Subject } from 'rxjs';
-import { HistoricalDataService, SourceKeysResponse } from './scenario-historicaldata-service';
-import { Subscription, MachineOverviewService, AssignedMachine } from '../scenario-machineoverview/scenario-machineoverview-service';
+import { Component, OnInit } from "@angular/core";
+import { Observable, of, Subject } from "rxjs";
+import {
+    HistoricalDataService,
+    SourceKeysResponse
+} from "./scenario-historicaldata-service";
+import {
+    Subscription,
+    MachineOverviewService,
+    AssignedMachine
+} from "../scenario-machineoverview/scenario-machineoverview-service";
 
 @Component({
-  selector: 'app-scenario-historicaldata',
-  templateUrl: './scenario-historicaldata.component.html',
-  styleUrls: ['./scenario-historicaldata.component.css']
+    selector: "app-scenario-historicaldata",
+    templateUrl: "./scenario-historicaldata.component.html",
+    styleUrls: ["./scenario-historicaldata.component.css"]
 })
 export class ScenarioHistoricaldataComponent implements OnInit {
     assignedMachines: Observable<AssignedMachine[]>;
-    sourceKeys: Observable<SourceKeysResponse>;
-    errorLoading$ = new Subject<boolean>();
+    sourceKeys: Observable<SourceKeysResponse[]>;
+    error$ = new Subject<boolean>();
+    loading$ = new Subject<boolean>();
+    loadingMachines$ = new Subject<boolean>();
 
-    constructor(private historicalDataService: HistoricalDataService) { }
+    constructor(private historicalDataService: HistoricalDataService) {
+        this.error$.next(false);
+        this.loading$.next(false);
+        this.loadingMachines$.next(false);
+    }
 
     ngOnInit() {
-        this.historicalDataService.getMachines().subscribe(machines => {
-            this.assignedMachines = of(machines);
-
-        }, error => {
-            console.error('could not load machines', error);
-            this.errorLoading$.next(true);
-        });
+        this.loadingMachines$.next(true);
+        this.historicalDataService.getMachines().subscribe(
+            machines => {
+                this.assignedMachines = of(machines);
+                this.loadingMachines$.next(false);
+            },
+            error => {
+                console.error("could not load machines", error);
+                this.loadingMachines$.next(false);
+                this.error$.next(true);
+            }
+        );
     }
-    
+
     selectedMachineChanged(machine: AssignedMachine) {
         console.log(machine);
+        this.loading$.next(true);
+        this.error$.next(false);
         this.sourceKeys = null;
         this.historicalDataService.getSourceKeys(machine.tmid).subscribe(
             sourceKeys => {
                 this.sourceKeys = of(sourceKeys);
+                this.loading$.next(false);
             },
             error => {
-                console.error('could not load sourceKeys', error);
-                this.errorLoading$.next(true);
+                console.error("could not load sourceKeys", error);
+                this.loading$.next(false);
+                this.error$.next(true);
             }
         );
     }
