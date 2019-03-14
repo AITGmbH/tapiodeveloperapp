74e4ff1f9734aca345b16b7aaf1f85f3298188a0
Lukas Ochsenreiter <Lukas.Ochsenreiter@aitgmbh.de>
2019-03-14 13:47:50 +0100

    Adds ngx datatable

--- src/web/src/app/scenario-historicaldata/scenario-historicaldata.component.ts
+++ src/web/src/app/scenario-historicaldata/scenario-historicaldata.component.ts
@@ -22,6 +22,13 @@ export class ScenarioHistoricaldataComponent implements OnInit {
     loading$ = new Subject<boolean>();
     loadingMachines$ = new Subject<boolean>();
 
+    rows = [
+        { name: "Austin", gender: "Male", company: "Swimlane" },
+        { name: "Dany", gender: "Male", company: "KFC" },
+        { name: "Molly", gender: "Female", company: "Burger King" }
+    ];
+    columns = [{ prop: "name" }, { name: "Gender" }, { name: "Company" }];
+
     constructor(private historicalDataService: HistoricalDataService) {
         this.error$.next(false);
         this.loading$.next(false);
--- src/web/src/app/shared/shared.module.ts
+++ src/web/src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-import { ScenarioComponent } from './components/scenario/scenario.component';
+import { ScenarioComponent } from "./components/scenario/scenario.component";
+import { NgxDatatableModule } from "@swimlane/ngx-datatable";
 
 library.add(faGithub);
 
@@ -13,15 +14,9 @@ library.add(faGithub);
  * Provides access to shared functionality.
  */
 @NgModule({
-  declarations: [ ScenarioComponent ],
-  imports: [
-    CommonModule,
-    FontAwesomeModule
-  ],
-  exports: [
-      ScenarioComponent,
-      FontAwesomeModule
-  ]
+    declarations: [ScenarioComponent],
+    imports: [CommonModule, FontAwesomeModule, NgxDatatableModule],
+    exports: [ScenarioComponent, FontAwesomeModule, NgxDatatableModule]
 })
 export class SharedModule {
     faGitHub = faGithub;
