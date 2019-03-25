1f336b26fd34cf659d0ea4f4c3f95220286b300c
Manuel Pfemeter <manuel.pfemeter@aitgmbh.de>
2019-03-25 09:58:58 +0100

    adds trivial tests

--- src/web.Tests.Unit/MachineOverviewServiceTests.cs
+++ src/web.Tests.Unit/MachineOverviewServiceTests.cs
@@ -3,15 +3,13 @@ using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-
 using Aitgmbh.Tapio.Developerapp.Web.Scenarios.MachineOverview;
 using Aitgmbh.Tapio.Developerapp.Web.Services;
 using FluentAssertions;
 using Moq;
-using Moq.Protected;
 using Xunit;
 
-namespace web.Tests.Unit
+namespace Aitgmbh.Tapio.Developerapp.Web.Tests.Unit
 {
     public class MachineOverviewServiceTests
     {
@@ -102,37 +100,6 @@ namespace web.Tests.Unit
                 Func<Task<SubscriptionOverview>> action = () => cut.GetSubscriptionAsync(CancellationToken.None);
                 await action.Should().ThrowAsync<HttpRequestException>();
             }
-        }
-    }
-
-    public static class MoqExtensions
-    {
-        private const string SendAsyncMethodName = "SendAsync";
-
-        public static void VerifySendAsyncWasInvokedExactlyOnce(this Mock<HttpMessageHandler> instance)
-        {
-            instance.Protected().Verify(
-                SendAsyncMethodName,
-                Times.Exactly(1),
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            );
-        }
-
-        public static Mock<HttpMessageHandler> SetupSendAsyncMethod(this Mock<HttpMessageHandler> instance, HttpStatusCode statusCode, string content)
-        {
-            instance.Protected()
-                .Setup<Task<HttpResponseMessage>>(
-                    SendAsyncMethodName,
-                    ItExpr.IsAny<HttpRequestMessage>(),
-                    ItExpr.IsAny<CancellationToken>()
-                )
-                .ReturnsAsync(new HttpResponseMessage {
-                    StatusCode = statusCode,
-                    Content = new StringContent(content)
-                })
-                .Verifiable();
-            return instance;
         }
     }
 }
