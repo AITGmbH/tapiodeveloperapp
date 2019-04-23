267329d44b74809ca339046186d7ce2e3a4d583f
Larissa Köhne <contact@larissakoehne.de>
2019-04-23 21:33:06 +0200

    integrates data into line chart

--- src/web/src/app/scenario-historicaldata/scenario-historicaldata.component.ts
+++ src/web/src/app/scenario-historicaldata/scenario-historicaldata.component.ts
@@ -1,83 +1,109 @@
 import { Component, OnInit } from "@angular/core";
-import { Observable, of, Subject } from "rxjs";
+import { Observable, of, Subject, BehaviorSubject } from "rxjs";
 import { SourceKeys } from "./source-keys.model";
 import { HistoricalDataService } from "./scenario-historicaldata.service";
-
+import { HistoricalDataResponseElement, HistoricItemData } from "./historical-data.model";
+import { filter, concatMap, tap, map, catchError } from "rxjs/operators";
+import * as moment from 'moment';
 @Component({
     selector: "app-scenario-historicaldata",
     templateUrl: "./scenario-historicaldata.component.html",
     styleUrls: ["./scenario-historicaldata.component.css"]
 })
 export class ScenarioHistoricaldataComponent implements OnInit {
+    private searchCriteria$ = new BehaviorSubject<ScenarionHistoricaldataSearchCriteria>({
+        data: {
+            // initial value, maybe add input
+            limit: 1000
+        }
+    });
     sourceKeys$: Observable<SourceKeys>;
     error$ = new Subject<boolean>();
     loading$ = new Subject<boolean>();
-    selectedMachine: string;
+    lineSeriesData: LineSeriesData[];
 
-    results = [
-        {
-            name: "TestData1",
-            series: [
-                {
-                    value: 4155,
-                    name: "2016-09-16T07:09:36.450Z"
-                },
-                {
-                    value: 4666,
-                    name: "2016-09-21T05:57:53.706Z"
-                },
-                {
-                    value: 5723,
-                    name: "2016-09-18T14:16:09.828Z"
-                },
-                {
-                    value: 2902,
-                    name: "2016-09-22T15:32:13.005Z"
-                },
-                {
-                    value: 2674,
-                    name: "2016-09-22T03:29:51.553Z"
-                }
-            ]
-        },
-        {
-            name: "TestData2",
-            series: [
-                {
-                    value: 4499,
-                    name: "2016-09-16T07:09:36.450Z"
-                },
-                {
-                    value: 6160,
-                    name: "2016-09-21T05:57:53.706Z"
-                },
-                {
-                    value: 4280,
-                    name: "2016-09-18T14:16:09.828Z"
-                },
-                {
-                    value: 6125,
-                    name: "2016-09-22T15:32:13.005Z"
-                },
-                {
-                    value: 6748,
-                    name: "2016-09-22T03:29:51.553Z"
-                }
-            ]
-        }
-    ];
     constructor(private readonly historicalDataService: HistoricalDataService) {
         this.error$.next(false);
         this.loading$.next(false);
     }
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.searchCriteria$
+            .pipe(
+                filter(this.filterIncompleteSearchCriteria()),
+                tap(this.setLoadingFlags()),
+                concatMap(this.getHistoricalDataBySearchCriteria()),
+                map(this.transformHistoricalDataToLineSeries())
+            )
+            .subscribe({
+                next: (data: LineSeriesData[]) => {
+                    this.loading$.next(false);
+                    this.lineSeriesData = data;
+                },
+                error: err => {
+                    this.error$.next(true);
+                    this.loading$.next(false);
+                    console.warn("hard error occured while fetching data: ", err);
+                }
+            });
+    }
+
+    private transformHistoricalDataToLineSeries(): (
+        value?: HistoricalDataResponseElement
+    ) => LineSeriesData[] {
+        return (responseElement?: HistoricalDataResponseElement): LineSeriesData[] => {
+            if (!responseElement) {
+                return [];
+            }
+            return [
+                {
+                    name: responseElement.key,
+                    series: responseElement.values
+                        // filter out non-numeric values
+                        .filter(
+                            (itemData: HistoricItemData) =>
+                                typeof itemData.vNum === "number"
+                        )
+                        // transform to series element
+                        .map((itemData: HistoricItemData) => ({
+                            value: itemData.vNum,
+                            name: moment(itemData.sts).toDate()
+                        }))
+                }
+            ];
+        };
+    }
+
+    private filterIncompleteSearchCriteria(): (
+        value: ScenarionHistoricaldataSearchCriteria
+    ) => boolean {
+        return (obj: ScenarionHistoricaldataSearchCriteria) => {
+            return !!(
+                obj &&
+                obj.machineId &&
+                obj.data.key &&
+                obj.data.from &&
+                obj.data.to &&
+                obj.data.limit
+            );
+        };
+    }
 
     public selectedMachineChanged(tmid: string) {
         this.loading$.next(true);
         this.error$.next(false);
         this.sourceKeys$ = null;
-        this.selectedMachine = tmid;
+        this.searchCriteria$.next({
+            // set new key
+            machineId: tmid,
+            data: {
+                // reset key, as the old one is invalid from now on.
+                key: null,
+                from: this.searchCriteria$.value.data.from,
+                to: this.searchCriteria$.value.data.to,
+                limit: this.searchCriteria$.value.data.limit
+            }
+        });
         this.historicalDataService.getSourceKeys(tmid).subscribe(
             sourceKeys => {
                 this.sourceKeys$ = of(sourceKeys);
@@ -93,7 +119,18 @@ export class ScenarioHistoricaldataComponent implements OnInit {
 
     public dateRangeChanged(dateRange: { dateStart: Date; dateEnd: Date }) {
         console.log("date range changed", dateRange);
+        this.searchCriteria$.next({
+            machineId: this.searchCriteria$.value.machineId,
+            data: {
+                key: this.searchCriteria$.value.data.key,
+                // TODO: change to Date type
+                from: dateRange.dateStart.toISOString(),
+                to: dateRange.dateEnd.toISOString(),
+                limit: this.searchCriteria$.value.data.limit
+            }
+        });
     }
+
     public radioChanged(event: Event) {
         if (event && event.target instanceof HTMLInputElement) {
             const ele = event.target as HTMLInputElement;
@@ -101,15 +138,53 @@ export class ScenarioHistoricaldataComponent implements OnInit {
             this.sourceKeySelected(ele.id);
         }
     }
+
     public sourceKeySelected(key: string) {
-        this.historicalDataService
-            .getHistoricalDataForKey(this.selectedMachine, {
+        this.searchCriteria$.next({
+            machineId: this.searchCriteria$.value.machineId,
+            data: {
                 key,
-                limit: 100 
-                /*from: "2019-04-17T08:15:29.1234Z",
-                to: "2019-04-18T08:15:29.1234Z"*/
-            })
-            .toPromise()
-            .then(console.log);
+                from: this.searchCriteria$.value.data.from,
+                to: this.searchCriteria$.value.data.to,
+                limit: this.searchCriteria$.value.data.limit
+            }
+        });
     }
+
+    private setLoadingFlags(): () => void {
+        return () => {
+            this.loading$.next(true);
+            this.error$.next(false);
+        };
+    }
+
+    private getHistoricalDataBySearchCriteria(): (
+        obj: ScenarionHistoricaldataSearchCriteria
+    ) => Observable<HistoricalDataResponseElement | null> {
+        return (obj: ScenarionHistoricaldataSearchCriteria) => {
+            return this.historicalDataService
+                .getHistoricalDataForKey(obj.machineId, {
+                    key: obj.data.key,
+                    from: obj.data.from,
+                    to: obj.data.to,
+                    limit: obj.data.limit
+                })
+                .pipe(
+                    catchError((err, caught) => {
+                        console.warn("error occured while fetching data: ", err);
+                        this.error$.next(true);
+                        return of(null);
+                    })
+                );
+        };
+    }
+}
+
+interface ScenarionHistoricaldataSearchCriteria {
+    machineId?: string;
+    data: { key?: string; from?: string; to?: string; limit?: number };
+}
+interface LineSeriesData {
+    name: string;
+    series: { value: number; name: string | Date | number }[];
 }
