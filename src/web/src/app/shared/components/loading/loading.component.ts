7c3129aa0859f6353ad5ab6b7d36647e946d3c31
Larissa Köhne <contact@larissakoehne.de>
2019-05-13 13:40:37 +0200

    removes unused loading component

--- src/web/src/app/shared/components/loading/loading.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loading',
-  templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.css']
-})
-export class LoadingComponent { }
