02144377970fe718918761e332c0f7997c980b98
Lukas Ochsenreiter <Lukas.Ochsenreiter@aitgmbh.de>
2019-05-14 16:19:09 +0200

    Adds directive to compare values

--- src/web/src/app/scenario-machinelivedata/scenario-machinelivedata-differ.directive.ts
+++ src/web/src/app/scenario-machinelivedata/scenario-machinelivedata-differ.directive.ts
@@ -8,6 +8,16 @@ import {
     DoCheck,
     Renderer2
 } from "@angular/core";
+import {
+    AnimationPlayer,
+    AnimationBuilder,
+    AnimationMetadata,
+    style,
+    state,
+    transition,
+    animate
+} from "@angular/animations";
+import { isEqual } from "lodash";
 
 @Directive({
     selector: "[live-data-update]"
@@ -15,20 +25,56 @@ import {
 export class LiveDataUpdateDirective implements DoCheck {
     @Input() liveDataValue = {};
     private differ: KeyValueDiffer<any, any>;
+    private animationPlayer: AnimationPlayer;
 
-    constructor(private differs: KeyValueDiffers, private host: ElementRef, private renderer: Renderer2) {
+    constructor(
+        private differs: KeyValueDiffers,
+        private host: ElementRef,
+        private animationBuilder: AnimationBuilder
+    ) {
         this.differ = this.differs.find(this.liveDataValue).create();
+        this.setupPlayer();
     }
 
     public ngDoCheck() {
-        const changes = this.differ.diff(this.liveDataValue);
-        this.renderer.removeClass(this.host.nativeElement, "updated");
+        const changes = this.differ.diff({ value: this.liveDataValue });
 
         if (changes) {
-            changes.forEachChangedItem((record: KeyValueChangeRecord<any, any>) => {
-                this.renderer.addClass(this.host.nativeElement, "updated");
-                console.log("forEachChangedItem - ", record.key, record.currentValue);
+            let invokeAnimation = false;
+            changes.forEachAddedItem((record: KeyValueChangeRecord<any, any>) => {
+                invokeAnimation = true;
             });
+            changes.forEachChangedItem((record: KeyValueChangeRecord<any, any>) => {
+                if (!isEqual(record.currentValue, record.previousValue)) {
+                    invokeAnimation = true;
+                }
+            });
+
+            if (invokeAnimation) {
+                this.animationPlayer.play();
+            }
         }
+    }
+
+    private setupPlayer(): void {
+        if (this.animationPlayer) {
+            this.animationPlayer.destroy();
+        }
+        const factory = this.animationBuilder.build(this.getAnimationMetadata());
+        this.animationPlayer = factory.create(this.host.nativeElement);
+    }
+
+    private getAnimationMetadata(): AnimationMetadata[] {
+        return [
+            style({
+                backgroundColor: "yellow"
+            }),
+            animate(
+                "1000ms ease-in",
+                style({
+                    backgroundColor: "inherit"
+                })
+            )
+        ];
     }
 }
