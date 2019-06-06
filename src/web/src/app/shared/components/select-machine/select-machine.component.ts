72c8de8714db12afded9eca24f5f5a38b6643569
Larissa Köhne <contact@larissakoehne.de>
2019-06-06 19:49:13 +0200

    changes style of select machine dropdown

--- src/web/src/app/shared/components/select-machine/select-machine.component.ts
+++ src/web/src/app/shared/components/select-machine/select-machine.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter, Input, ViewEncapsulation } from "@angular/core";
 import { Observable, of, Subscription as rxSubscription } from "rxjs";
 import { AssignedMachine } from "../../models/assigned-machine.model";
 import { MachineOverviewService } from "src/app/scenario-machineoverview/scenario-machineoverview.service";
@@ -9,7 +9,8 @@ import { NgOption } from "@ng-select/ng-select";
 @Component({
     selector: "app-select-machine",
     templateUrl: "select-machine.component.html",
-    styleUrls: ["./select-machine.component.css"]
+    styleUrls: ["./select-machine.component.css"],
+    encapsulation: ViewEncapsulation.None
 })
 export class SelectMachineComponent implements OnInit {
     public items$: Observable<Array<AssignedMachine | NgOption>>;
