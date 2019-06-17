ec9af91929158bc88c5f0edb54de560334143fa7
Manuel Pfemeter <manuel.pfemeter@aitgmbh.de>
2019-06-17 16:32:02 +0200

    improves nav bar styling

--- src/web/src/app/app.component.ts
+++ src/web/src/app/app.component.ts
@@ -10,24 +10,23 @@ export class AppComponent implements OnInit {
     title = "developerapp";
 
     ngOnInit(): void {
-            // Get all "navbar-burger" elements
-        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+        // Get all "navbar-burger" elements
+        const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
 
         // Check if there are any navbar burgers
-        if ($navbarBurgers.length > 0) {
+        if (navbarBurgers.length > 0) {
 
             // Add a click event on each of them
-            $navbarBurgers.forEach( (el: any) => {
-                el.addEventListener('click', () => {
+            navbarBurgers.forEach( (el: any) => {
+                el.addEventListener("click", () => {
 
                     // Get the target from the "data-target" attribute
                     const target = el.dataset.target;
                     const $target = document.getElementById(target);
 
                     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                    el.classList.toggle('is-active');
-                    $target.classList.toggle('is-active');
-
+                    el.classList.toggle("is-active");
+                    $target.classList.toggle("is-active");
                 });
             });
         }
