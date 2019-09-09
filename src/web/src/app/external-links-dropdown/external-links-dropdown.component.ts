d532a6f9f607d9fa1d31f93bd98be7b7e4468b3e
Lukas Ochsenreiter <Lukas.Ochsenreiter@aitgmbh.de>
2019-09-09 15:55:25 +0200

    Changes parts for bulma usage

--- src/web/src/app/external-links-dropdown/external-links-dropdown.component.ts
+++ src/web/src/app/external-links-dropdown/external-links-dropdown.component.ts
@@ -1,13 +1,18 @@
 import { Component } from "@angular/core";
+import { NavigationService } from "../shared/services/navigation.service";
 
 /***
  * Displays the developer links in a dropdown menu.
  */
 @Component({
-  selector: "app-external-links-dropdown",
-  templateUrl: "./external-links-dropdown.component.html",
-  styleUrls: ["./external-links-dropdown.component.scss"]
+    selector: "[app-external-links-dropdown]",
+    templateUrl: "./external-links-dropdown.component.html",
+    styleUrls: ["./external-links-dropdown.component.scss"]
 })
 export class ExternalLinksDropdownComponent {
-  constructor() { }
+    constructor(private readonly navigationService: NavigationService) {}
+
+    public selectEntry() {
+        this.navigationService.toggleMenu();
+    }
 }
--- src/web/src/app/scenario-navigation/scenario-navigation.component.ts
+++ src/web/src/app/scenario-navigation/scenario-navigation.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { ScenarioNavigationService } from "./scenario-navigation.service";
 import { ScenarioEntry } from "../shared/models/scenario-entity.model";
+import { NavigationService } from "../shared/services/navigation.service";
 
 /**
  * Displays the available scenario menu entries.
  */
 @Component({
-    selector: "app-scenario-navigation",
+    selector: "[app-scenario-navigation]",
     templateUrl: "./scenario-navigation.component.html",
     styleUrls: ["./scenario-navigation.component.css"]
 })
@@ -15,11 +16,18 @@ export class ScenarioNavigationComponent implements OnInit {
     private readonly scenarioEntries: BehaviorSubject<ScenarioEntry[]> = new BehaviorSubject<ScenarioEntry[]>([]);
     scenarioEntries$ = this.scenarioEntries.asObservable();
 
-    constructor(private readonly scenarioNavigationService: ScenarioNavigationService) {}
+    constructor(
+        private readonly scenarioNavigationService: ScenarioNavigationService,
+        private readonly navigationService: NavigationService
+    ) {}
 
     ngOnInit() {
         this.scenarioNavigationService.getEntries().subscribe(data => {
             this.scenarioEntries.next(data);
         });
+    }
+
+    public selectEntry() {
+        this.navigationService.toggleMenu();
     }
 }
